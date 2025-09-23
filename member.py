4a25d0152331498b48b30a37ec16fbd43343df5d
Hashem973 <hashimhamoud2007@gmail.com>
2025-09-23 14:59:46 +0300

    Delete member.py

--- member.py
+++
@@ -1,9 +0,0 @@
-class member :
-    def __init__(self , name , membership_id , borrowed_books):
-        self.name = name
-        self.membership = membership_id
-        self.borrowed_books = borrowed_books
-    def borrow_book(self , book) :
-      print(f"{self.name}brrowed'{book.title}'") 
-    def return_book(self , book) :
-       print(f"{self.name}returned'{book.title}'")
