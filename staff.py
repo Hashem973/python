b22d0623930bba29720ab5f62ee78935d822f99f
Hashem973 <hashimhamoud2007@gmail.com>
2025-09-23 15:00:38 +0300

    Delete staff.py

--- staff.py
+++
@@ -1,5 +0,0 @@
-class staffmember : 
-    def __init__(self , staff_id):
-     self.staff_id = staff_id
-     def add_book (self , memer , book) :
-        print(f"staff{self.name}added book '{book.title}'")
