46b51260cbd8b82749d3b8272763678046b1cea5
Hashem973 <hashimhamoud2007@gmail.com>
2025-09-23 15:00:22 +0300

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-from book import book
-from member import member
-book1 = book("max" , "ahmed" , 333 , True)
-book2 = book("club", "omar" , 222 , False)
-member1 = member("ali" , 777 , 4)
-member2 = member("mohamed" , 666 , 3)
